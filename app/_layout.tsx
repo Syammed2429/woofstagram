c09d31a3affdca02fc7dd5aeda12b2811f62f6bd
Dada Khalandar <kerry.blig12@gmail.com>
2025-07-06 17:23:00 +0800

    refactor: remove platform-specific setup and unused functions from RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,25 +2,13 @@ import "~/global.css";
 
 import { Stack } from "expo-router";
 import * as React from "react";
-import { Appearance, Platform } from "react-native";
-import { setAndroidNavigationBar } from "~/lib/android-navigation-bar";
-import { useColorScheme } from "~/lib/useColorScheme";
 
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
 } from "expo-router";
 
-const usePlatformSpecificSetup = Platform.select({
-  web: useSetWebBackgroundClassName,
-  android: useSetAndroidNavigationBar,
-  default: noop,
-});
-
 export default function RootLayout() {
-  usePlatformSpecificSetup();
-  const { isDarkColorScheme } = useColorScheme();
-
   return (
     <Stack>
       <Stack.Screen
@@ -32,24 +20,6 @@ export default function RootLayout() {
       />
     </Stack>
   );
-}
-
-const useIsomorphicLayoutEffect =
-  Platform.OS === "web" && typeof window === "undefined"
-    ? React.useEffect
-    : React.useLayoutEffect;
-
-function useSetWebBackgroundClassName() {
-  useIsomorphicLayoutEffect(() => {
-    // Adds the background color to the html element to prevent white background on overscroll.
-    document.documentElement.classList.add("bg-background");
-  }, []);
-}
-
-function useSetAndroidNavigationBar() {
-  React.useLayoutEffect(() => {
-    setAndroidNavigationBar(Appearance.getColorScheme() ?? "light");
-  }, []);
 }
 
 function noop() {}
